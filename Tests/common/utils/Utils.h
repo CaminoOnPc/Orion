d40cd4bcd4c9366459485ba58755f9460335b806
CAMINOonPC <35667100+CAMINOonPC@users.noreply.github.com>
2020-11-02 18:56:53 +0200

    Removed glm

--- Tests/common/utils/Utils.h
+++ Tests/common/utils/Utils.h
@@ -25,39 +25,4 @@ namespace Utils
 	{
 		return boost::filesystem::current_path().make_preferred().string();
 	}
-
-	static std::string convert_to_utf_8(const std::wstring& wstr)
-	{
-		const int bufferSize = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
-		const std::unique_ptr<char[]> buffer(new char[bufferSize]);
-		WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, buffer.get(), bufferSize, nullptr, nullptr);
-		return std::string(buffer.get());
-	}
-
-	static std::wstring convert_to_utf_16(const std::string& str)
-	{
-		const int bufferSize = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
-		const std::unique_ptr<wchar_t[]> buffer(new wchar_t[bufferSize]);
-		MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buffer.get(), bufferSize);
-		return std::wstring(buffer.get());
-	}
-
-	template<typename T> static constexpr T get_mipmap_levels(T width, T height)
-	{
-		T levels = 1;
-		while ((width | height) >> levels) {
-			++levels;
-		}
-		return levels;
-	}
-
-	template<typename T> static constexpr T round_to_power_of_two(T value, int POT)
-	{
-		return (value + POT - 1) & -POT;
-	}
-
-	template<typename T> static constexpr bool is_power_of_two(T value)
-	{
-		return value != 0 && (value & (value - 1)) == 0;
-	}
 }
